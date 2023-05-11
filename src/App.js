019404511cb54d2393656abb2a1bd53f5f83634b
Wondem-Abebaw <128637148+Wondem-Abebaw@users.noreply.github.com>
2023-05-11 10:49:10 +0300

    Netflix-Clone

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import Row from "./components/ROW/Row";
+import requests from "./requests";
+import Banner from "./components/Banner/Banner";
+import Nav from "./components/Nav/Nav";
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+      <Row title="Top Rated Movies" fetchURL={requests.fetchTopRatedMovies} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
